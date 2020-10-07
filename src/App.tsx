e1c903b88f751114ad753ff7fc106e11092a017c
fathurifki <rifkifathu@gmail.com>
2020-10-07 16:56:10 +0700

    add base layout root

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen m-auto max-w-md bg-gray-300">
+      <div className="flex flex-col h-screen">
+        <p className="font-bold text-white">Test Root App</p>
+      </div>
     </div>
   );
 }
